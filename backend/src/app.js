4d9c8c0e3b82212b26a1f422576b5c2cda434d91
davidruc <davidarueda18@gmail.com>
2023-07-17 17:15:32 -0500

    feat: :sparkles: introducción del app para la creación del servidor

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+
+
+
+const config = JSON.parse(process.env.MY_CONFIG);
+app.listen(config, ()=>console.log(`http://${config.hostname}:${config.port}`));
+
+
+export default app;
